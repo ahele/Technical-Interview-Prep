21fac8636988bb990e34d06715fab30750d38cc6
ahele <119096569+ahele@users.noreply.github.com>
2024-06-27 20:32:22 +0000

    Unit 4 session 1 added

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Write a function get_last() that takes in a list as a parameter and returns the 
 #     return last_member
 
 # print(get_last([3,1,6,7,5]))
-
 '''
 Write a function counter() that uses the range function to print numbers between 1 and a given stop value (inclusive).
 '''
@@ -17,7 +16,6 @@ Write a function counter() that uses the range function to print numbers between
 #         print(i)
 
 # counter(8)
-
 '''
 Write a function sum_ten() that returns the sum of numbers from 1 to 10.
 '''
@@ -30,7 +28,6 @@ Write a function sum_ten() that returns the sum of numbers from 1 to 10.
 #     print(sum_tt)
 
 # sum_ten()
-
 '''
 Write a function sum_positive_range() that returns the sum of numbers from 1 to a given stop value (inclusive).
 '''
@@ -42,7 +39,6 @@ Write a function sum_positive_range() that returns the sum of numbers from 1 to 
 #     print(sum)
 
 # sum_positive_range(7)
-
 '''
 Write a function sum_range() that returns the sum of numbers from a given start value to a given stop value (inclusive).
 '''
@@ -55,7 +51,6 @@ Write a function sum_range() that returns the sum of numbers from a given start 
 #     print(sum)
 
 # sum_range(3, 9)
-
 '''
 Write a function print_negatives() that takes a list of integers lst and prints all negative numbers in the list.
 '''
@@ -65,10 +60,9 @@ Write a function print_negatives() that takes a list of integers lst and prints 
 #             print(i)
 
 # print_negatives([3,-2,2,1,-5])
-
 '''
 Write a function find_divisors() that takes in an integer n as a parameter that returns a list of all divisors of n.
-'''       
+'''
 # def find_divisors(n):
 #     divisors = []
 #     for i in range(1, n+1):
@@ -77,12 +71,11 @@ Write a function find_divisors() that takes in an integer n as a parameter that 
 #     print(divisors)
 
 # find_divisors(6)
-
 '''
 Write a function fizzbuzz() that takes in an integer n as a parameter and prints the numbers from 1 to n.
 For multiples of 3, print "Fizz" instead of the number.
 For multiples of 5, print "Buzz" instead of the number.
-'''  
+'''
 
 # def fizzbuzz(n):
 #     for i in range(1, n+1):
@@ -92,10 +85,9 @@ For multiples of 5, print "Buzz" instead of the number.
 #             print("Buzz")
 #         else:
 #             print(i)
-#     return 
+#     return
 
 # fizzbuzz(13)
-
 '''
 Write a function print_indices() that takes in an integer list lst as a parameter and prints out the index of each item in the given list.
 Use the function range() to loop through the list indices.
@@ -108,7 +100,6 @@ Use the function range() to loop through the list indices.
 #     return
 
 # print_indices([5,1,3,8,2])
-
 '''
 Write a function linear_search() that takes in a list lst and value target as parameters. The function returns the index of target in lst if found. If target is not found in lst, return -1.
 '''
@@ -147,7 +138,6 @@ Write a function is_subsequence() that takes in a list of integers lst and a lis
 # lst = [5, 1, 22, 25, 6, -1, 8, 10]
 # sequence = [1, 6, -1, 10]
 # print(is_subsequence(lst, sequence))
-
 '''
 Write a function create_dictionary() that takes in a list of keys and a list of values as parameters. The function returns a dictionary where each item in keys is paired with a corresponding item in values.
 
@@ -157,11 +147,10 @@ keys[i] should be paired with values[i] in the dictionary where 0 <= i <= len(ke
 # def create_dictionary(keys, values):
 #   new_dict=dict(zip(keys,values))
 #   return new_dict
-    
+
 # keys = ["peanut", "dragon", "star", "pop", "space"]
 # values = ["butter", "fly", "fish", "corn", "ship"]
 # print(create_dictionary(keys, values))
-
 '''
 Write a function print_pair() that takes in a dictionary dictionary and a key target as parameters. The function looks for the target and when found, it prints the key and it's associated value as "Key: <key>" followed by "Value: <value>". If target is not in dictionary, print "That pair does not exist!".
 '''
@@ -171,13 +160,12 @@ Write a function print_pair() that takes in a dictionary dictionary and a key ta
 #       print("Key:", target)
 #       print("Value", dictionary.get(target))
 #     else:
-#       print("That pair does not exist!") 
+#       print("That pair does not exist!")
 
 # dictionary = {"spongebob": "squarepants", "patrick": "star", "squidward": "tentacles"}
 # print_pair(dictionary, "patrick")
 # print_pair(dictionary, "plankton")
 # print_pair(dictionary, "spongebob")
-
 '''Write a function keys_v_values() that takes in a dictionary dictionary whose keys and values are both integers. The function should find the sum of all keys in the dictionary and the sum of all values.
 If the sum of all keys is greater than the sum of all values, the function should return the string "keys".
 If the sum of all values is greater than the sum of all keys, the function should return the string "values".
@@ -201,7 +189,6 @@ If keys and values have equal sums, the function should return the string "balan
 # dictionary2 = {100:10, 200:20, 300:30, 400:40, 500:50, 600:60}
 # greater_sum = keys_v_values(dictionary2)
 # print(greater_sum)
-
 '''
 Write a function restock_inventory() that updates an inventory dictionary based on a restock list. It accepts two parameters:
 
@@ -230,7 +217,6 @@ If an item in restock_list is not present in the current_inventory, it should be
 # }
 
 # print(restock_inventory(current_inventory,restock_list))
-
 '''
 Write a function calculate_gpa() that calculates the grade point average (GPA) for a student based on their course grades and returns the gpa as a float. The function takes in a dictionary report_card as a parameter where each key-value pair represents a course and the grade received in that course respectively. The grades are represented as strings ("A", "B", "C", "D", "F") and each grade corresponds to a certain number of grade points:
 
@@ -260,7 +246,6 @@ A GPA is calculated by finding the average of all grade points.
 
 # report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
 # print(calculate_gpa(report_card))
-
 '''
 Imagine you are working on a book review software like Goodreads. Write a function named highest_rated() that returns the book with the highest rating.
 
@@ -290,7 +275,6 @@ The function should take in a list of dictionaries named books as a parameter. E
 # ]
 
 # print(highest_rated(books))
-
 '''Write a function index_to_value_map() that takes in a list lst and returns a dictionary that maps the index of each element in lst to its value.
 '''
 
@@ -300,7 +284,6 @@ The function should take in a list of dictionaries named books as a parameter. E
 
 # lst = ["apple", "banana", "cherry"]
 # print(index_to_value_map(lst))
-
 '''
 Write a function cast_vote() that records a vote for a candidate in an election. The function accepts a dictionary votes that maps candidates to their current number of votes and a string candidate that represents the candidate the user would like to vote for. If the candidate doesn't exist, add them to the dictionary. The function should return the updated dictionary.
 '''
@@ -317,7 +300,6 @@ Write a function cast_vote() that records a vote for a candidate in an election.
 # print(votes)
 # cast_vote(votes, "Gina")
 # print(votes)
-
 '''Write a function that takes in two dictionaries, dict1 and dict2 and finds all keys common to both dictionaries. The function returns a list of common keys.'''
 
 # def common_keys(dict1, dict2):
@@ -331,7 +313,6 @@ Write a function cast_vote() that records a vote for a candidate in an election.
 # dict2 = {"b": 4, "c": 5, "d": 6}
 # common_list = common_keys(dict1, dict2)
 # print(common_list)
-
 '''
 Given a dictionary tasks where keys are task names and values are priorities (1-10, where 10 is the highest priority), write a function get_highest_priority_task() that removes the task with the highest priority from the dictionary and returns its name.
 If two tasks have the same priority, return the task that comes first in the alphabet.
@@ -358,7 +339,6 @@ If two tasks have the same priority, return the task that comes first in the alp
 # print(perform_task)
 
 # print(tasks)
-
 '''
 Write a function that takes in a list of integers nums and counts the number of occurrences of each integer. The function returns the result as a dictionary with integers as keys and their counts as values.
 '''
@@ -373,7 +353,6 @@ Write a function that takes in a list of integers nums and counts the number of 
 
 # nums = [1, 2, 2, 3, 3, 3, 4]
 # print(count_occurrences(nums))
-
 '''
 Write a function find_majority_element() that takes in a list of integers elements and finds the majority element in the list. A majority element is an element that appears more than n/2 times where n is the size of the list. If there is no majority element, return None.
 '''
@@ -397,7 +376,6 @@ Write a function find_majority_element() that takes in a list of integers elemen
 
 # elements = [2, 2, 1, 1, 1, 2, 2]
 # print(find_majority_element(elements))
-
 '''
 Write a function divideList() that takes in an integer list nums consisting of 2*n integers as parameters. The function divides nums into n pairs such that:
 
@@ -423,13 +401,12 @@ Return True if nums can be divided into n pairs, otherwise return False.
 
 # nums = [3,2,3,2,2,2]
 # print(divideList(nums))
-        
+
 # def count_mississippi(limit):
 #   for num in range(1, limit +1):
 #     print( f"{num} mississippi")
 
 # count_mississippi(5)
-
 '''
    Write a function swap_ends() that accepts a string my_str as a parameter and returns a new string where the first and last characters from my_str are swapped.
 '''
@@ -449,12 +426,11 @@ Return True if nums can be divided into n pairs, otherwise return False.
 #         start = my_str[-1]
 #         mid = my_str[1:(len(my_str)-1)]
 #         new_str = start + mid + end
-#     return new_str 
+#     return new_str
 
 # my_str = "boat"
 # swapped = swap_ends(my_str)
 # print(swapped)
-
 '''
 Write a function is_pangram() that takes in a string my_str as a parameter and returns True if the string is a pangram and False if not. A pangram is a sentence containing every letter in the English alphabet.
 '''
@@ -473,7 +449,6 @@ Write a function is_pangram() that takes in a string my_str as a parameter and r
 
 # str2 = "The dog jumped"
 # print(is_pangram(str2))
-
 '''
    Write a function reverse_string() that takes a string my_str as a parameter and returns the string reversed.
 '''
@@ -486,7 +461,6 @@ Write a function is_pangram() that takes in a string my_str as a parameter and r
 
 # my_str = "live"
 # print(reverse_string(my_str))
-
 '''
    Write a function first_unique_char() that given a string my_str as a parameter, it finds the first non-repeating character in it and returns its index. If it does not exist, then return -1.
 '''
@@ -512,7 +486,6 @@ Write a function is_pangram() that takes in a string my_str as a parameter and r
 
 # str3 = "aabb"
 # print(first_unique_char(str3))
-
 '''
 Write a function min_distance() that takes in a list of strings words and two strings word1 and word2' as parameters. The function should return the minimum distance between word1 and word2 in the list of words. The distance between one word and an adjacent word in the list is 1.
 '''
@@ -529,12 +502,11 @@ Write a function min_distance() that takes in a list of strings words and two st
 # dist1 = min_distance(words, "quick", "jumped")
 # dist2 = min_distance(words, "the", "jumped")
 # print(dist1)
-# print(dist2)   
+# print(dist2)
 
 # words2 = ["code", "path", "code", "contribute",  "practice"]
 # dist3 = min_distance(words2, "code", "practice")
 # print(dist3)
-
 '''
 Write a function sum_of_number_strings() that takes in a list of strings nums. Each string is a representations of integers. The function should return the sum of these strings as an integer.
 '''
@@ -548,8 +520,6 @@ Write a function sum_of_number_strings() that takes in a list of strings nums. E
 # nums = ["10", "20", "30"]
 # sum = sum_of_number_strings(nums)
 # print(sum)
-
-
 '''
 Write a function remove_duplicates() that takes in a sorted list of integers nums as a parameter and removes all duplicates in the list. The function returns the modified list.
 '''
@@ -561,10 +531,9 @@ Write a function remove_duplicates() that takes in a sorted list of integers num
 #     else:
 #       i += 1
 #   return nums
-      
+
 # nums = [1,1,1,2,3,4,4,5,6,6]
 # print(remove_duplicates(nums))
-
 '''
 Write a function reverse_only_letters() that takes in a string s as a parameter. The function reverses the order of the letters in the string and returns the new string. Non-letter characters should remain in their original positions.
 '''
@@ -579,8 +548,6 @@ Write a function reverse_only_letters() that takes in a string s as a parameter.
 # s = "a-bC-dEf-ghIj"
 # reversed_s = reverse_only_letters(s)
 # print(reversed_s)
-
-
 '''
 Write a function longest_uniform_substring() that takes in a string s and returns the length of the longest uniform substring. A uniform substring consists of a single repeated character.
 '''
@@ -607,7 +574,6 @@ Write a function longest_uniform_substring() that takes in a string s and return
 # s2 = "abcdef"
 # l2 = longest_uniform_substring(s2)
 # print(l2)
-
 '''
 In the game League of Legends, Teemo attacks his enemy Ashe with poison arrows. Write a function find_poisoned_duration() that takes in two parameters: time_series (the time at which Teemo's attacks hits Ashe) and time_duration (the duration of the poisoning effect). The function returns the total time that Ashe is in a poisoned condition.
 
@@ -623,14 +589,13 @@ If Teemo hits Ashe while she is still poisoned, the poison's duration starts ove
 #     if time_difference <= duration:
 #       total_time += time_difference
 #     elif time_difference > duration:
-#       total_time += duration 
+#       total_time += duration
 #   new_time = total_time + duration
 #   return new_time
-  
+
 # time_series = [1,4,9]
 # damage = find_poisoned_duration(time_series, 3)
 # print(damage)
-
 '''
 Write a function sum_of_unique_elements() that takes in two lists of integers, lst1 and lst2, as parameters and returns the sum of the elements that are unique in lst1.
 
@@ -648,7 +613,7 @@ it does not appear in lst2
 #       unique_sum += lst1[i]
 #   return unique_sum
 
-# lstA = [1, 2 ,3, 4] 
+# lstA = [1, 2 ,3, 4]
 # lstB = [3, 4, 5, 6]
 # lstC = [7, 7, 7, 7]
 
@@ -657,4 +622,116 @@ it does not appear in lst2
 
 # sum2 = sum_of_unique_elements(lstC, lstB)
 # print(sum2)
-      
+'''
+Write a function is_prime() that takes in a positive integer n and returns True if it is a prime number and False otherwise. A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+'''
+
+# def is_prime(n):
+#    factors = []
+#    for i in range(1, n):
+#       if n % i == 0:
+#          factors.append(i)
+
+#    return len(factors) < 2
+
+# print(is_prime(5))
+# print(is_prime(12))
+# print(is_prime(9))
+'''
+Write a function reverse_list() that takes in a list lst and returns elements of the list in reverse order. The list should be reversed in-place without using list slicing (e.g. lst[::-1]).
+'''
+
+# def reverse_list(lst):
+#    i = 0
+#    j = len(lst)-1
+#    new_list=list(lst)
+#    while i < j:
+#       new_list[i] = lst[j]
+#       new_list[j] = lst[i]
+#       i += 1
+#       j -= 1
+
+#    return new_list
+
+# lst = [1, 2, 3, 4, 5]
+# print(reverse_list(lst))
+'''
+The reverse_list() problem can also be solved without using the two pointer technique (as you may have seen it in previous units)! Evaluate the time and space complexity of your two-pointer solution.
+'''
+
+# def reverse_list(lst):
+#     reversed_lst = lst[::-1]
+#     return reversed_lst
+
+# lst = [1, 2, 3, 4, 5]
+# print(reverse_list(lst))
+'''
+Write a function sort_list_by_parity() that takes in an integer list nums as a parameter and moves all the even integers at the beginning of the list followed by all the odd integers. The function returns any list that satisfies this condition.
+'''
+
+# def sort_array_by_parity(nums):
+#    new_lst = [i for i in nums if i % 2 == 0]
+#    for j in nums:
+#       if j not in new_lst:
+#          new_lst.append(j)
+#    return new_lst
+
+# nums = [3,1,2,4]
+# nums2 = [0]
+# print(sort_array_by_parity(nums))
+# print(sort_array_by_parity(nums2))
+'''
+Write a function first_palindrome() that takes in a list of strings words as a parameter and returns the first palindromic string in the list. A string is palindromic if it reads the same forward and backward. If there is no such string, return an empty string ""
+'''
+
+
+# def palindrome_element(n):
+#    n_new = list(n)
+#    new_n = list(n)
+#    i = 0
+#    j = len(n) - 1
+#    while i < j:
+#       new_n[i] = n_new[j]
+#       i += 1
+#       j -= 1
+
+#    return "".join(new_n)
+
+
+# def first_palindrome(words):
+#    palindrome_words = []
+#    for i in words:
+#       if palindrome_element(i) == i:
+#          palindrome_words.append(i)
+   
+#    if not palindrome_words:
+#       return ""
+#    else:
+#       return palindrome_words[0]
+   
+
+# words = ["abc", "car", "ada", "racecar", "cool"]
+# palindrome1 = first_palindrome(words)
+# print(palindrome1)
+
+# words2 = ["abc","racecar","cool"]
+# palindrome2 = first_palindrome(words2)
+# print(palindrome2)
+
+# words3 = ["abc", "def", "ghi"]
+# palindrome3 = first_palindrome(words3)
+# print(palindrome3)
+
+# def remove_duplicates(nums):
+#     i = 0
+#     while i < len(nums)-1:
+#        if nums[i] == nums[i+1]:
+#           nums.pop(i+1)
+#        else:
+#           i += 1
+#     return nums
+   
+# nums = [1,1,2,3,4,4,4,5]
+# print(nums)
+# print(remove_duplicates(nums))
+# print(nums) # same list
